b170608d6911f7d04fa2ebfb423529a36dc0a5b6
Marianoantar <marianoantar@gmail.com>
2024-09-12 17:57:54 -0300

    Se agregó metodos de Post, Put y Delete

--- src/product.controller.ts
+++ src/product.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { ProductService } from "./services/product.service";
 import { ApiTags } from "@nestjs/swagger";
+import { Producto_model } from "./interfaces/productos";
 
 @ApiTags('products')
 @Controller('product')
@@ -11,5 +12,29 @@ constructor(private productService: ProductService){}
  async getProducts(): Promise<any> {
     return this.productService.getProducts();
  }
+
+ @Put(':id')
+ async updateProduct(@Param('id') id:string, @Body() product: Producto_model): Promise<any> {
+    const idNum = parseInt(id);
+    const res = await this.productService.put(idNum, product);
+    return res;
+ }
+ 
+ @Post('')
+async newProduct(@Body() product: Producto_model): Promise<any> {
+    const res = await this.productService.post(product);
+    return res;
+
+}
+
+ @Delete(':id')
+ async deleteProduct(@Param('id') id: string): Promise<any> {
+    const idNum = parseInt(id);
+    const res = await this.productService.delete(idNum);
+    return res;
+ }
+
+
+
 
 }
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Categoria_model } from 'src/interfaces/categorias';
+import { findIndex } from 'rxjs';
+import { Producto_model } from 'src/interfaces/productos';
 
 @Injectable()
 export class ProductService {
@@ -18,6 +19,129 @@ export class ProductService {
           }        
 
         //   return 'TODOS LOS PRODUCTOS \n Proximamente subidos a la nube.'
+    }
+
+    async delete(idProduct: number): Promise<any> {
+        try {
+            // * resuelve id Category
+            const idCategory = (idProduct / 100) | 0;
+
+            // * Carga datos en database
+            const fs = require('fs');
+            let database = await this.getProducts();
+
+            // * resuelve indice de Category
+            const indexCategory = database.findIndex(cat => cat.id === idCategory);
+
+            // * borra producto y guardar
+            database[indexCategory].productos = database[indexCategory].productos.filter(prod => prod.id !== idProduct);
+            await fs.promises.writeFile('./src/data/database.json', JSON.stringify(database), 'utf-8');
+
+            return {
+                "status": "success",
+                "message": "Registro eliminado exitosamente",
+                "deletedId": idProduct // ID del registro eliminado
+            };
+        } catch (error) {
+            return {
+                statusCode: error.status,
+                message: 'Error: ' + error.message
+            };
+
+            
+        }
+    }
+
+    async post(product: Producto_model): Promise<any> {
+        try {
+            // * resuelve id Category
+            const idCategory = (product.id / 100) | 0;
+            console.log('id categoria: ', idCategory);
+ 
+            // * Carga datos en database
+            const fs = require('fs');
+            let database = await this.getProducts();
+
+            // * resuelve indice de Category
+            const indexCategory = database.findIndex(cat => cat.id === idCategory);
+            console.log('index categoria: ', indexCategory);
+
+            // // * carga productos con los productos de Category
+            // let productos = database[indexCategory].productos;
+            
+            // * Agregar producto a Productos
+            database[indexCategory].productos.push(product);
+            // productos.push(product);
+            // console.log('Productos después de agregar: ', productos.nombre);
+
+            // * Guardar database
+            await fs.promises.writeFile('./src/data/database.json', JSON.stringify(database), 'utf-8');
+
+            return {
+                statusCode: 201,
+                message: 'Recurso creado con éxito'
+            };
+        } catch (error) {
+            return {
+                statusCode: error.status,
+                message: 'Error: ' + error.message
+            };
+        }
+    }
+
+    async put(id:number, product: Producto_model): Promise<any> {
+        try {
+            const idCategory = (id / 100) | 0;
+            console.log('id categoria: ', idCategory);
+            const indexCategory = idCategory - 1;
+            console.log('index categoria: ', indexCategory);
+
+            const fs = require('fs');
+            let database = await this.getProducts();
+            // console.log((database));
+            let productos = database[indexCategory].productos;
+            // console.log('array products: ',productos);
+
+            const indice = productos.findIndex((p => p.id === id ));
+            console.log('indice del producto : ', indice);
+            console.log('producto A Modificar: ',productos[indice]);
+            if (indice !== -1) {
+                productos.splice(indice, 1, product);
+                console.log('producto Modificado: ',productos[indice]);
+            } else {
+                console.error('No se encontró el producto para modificar');
+                return {
+                    statusCode: 404,
+                    message: 'No se encontró el producto para modificar'
+                };
+            }
+
+            database[indexCategory].productos = productos;
+            // console.log(database);
+
+            await fs.promises.writeFile('./src/data/database.json', JSON.stringify(database), 'utf-8');
+
+            // const indiceProducto = this.productos.findIndex(p => p.id === this.producto.id);
+            // if (indiceProducto !== -1){
+            //   this.productos.splice(indiceProducto, 1, this.producto);
+            // }
+
+            // database.categorias[idCategory].products.push(product);
+            // database.categorias[idCategory].productos.push(product);
+            // await fs.promises.writeFile('./src/data/database.json', JSON.stringify(database), 'utf-8');
+    
+            return {
+                statusCode: 201,
+                message: 'Recurso creado con éxito'
+            };
+        } catch (error) {
+            return {
+                statusCode: error.status,
+                message: 'Error: ' + error.message
+            };
+            
+        }
+        // database.categorias[idCategory].products.push(product);
     }
 
 }
